46051db5fe71686f9361461eed4f7a2b23c1cf2c
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-02-06 12:22:39 +0700

    complete getMatch-PlayFriendContrller

--- app/Http/Controllers/PlayFriendController.php
+++ app/Http/Controllers/PlayFriendController.php
@@ -6,11 +6,31 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Input;
+use DB;
 class PlayFriendController extends Controller
 {
     public function getMatch(){
-        $friend = Input::get("friend");
-        $friend = 
+        $friends = json_decode(Input::get("friends"));
+        $user = Input::get("facebook_id");
+        $match = array();
+        foreach((array)$friends as $friend){
+            $result = DB::select('
+                SELECT reserves.*, stadiums.name as stadiumName, stadiums.latitude as latitude, stadiums.longitude as longitude, fields.name as fieldname, stadiums.image as image
+                From reserves, stadiums, fields, joins, users
+                WHERE
+                  reserves.field_id = fields.id AND
+                  stadiums.id = fields.stadium_id AND
+                  reserves.facebook_id = users.facebook_id AND
+                  users.facebook_id != :self1 AND
+                  joins.reserve_id = reserves.id AND
+                  NOT EXISTS (SELECT joins.facebook_id FROM joins WHERE joins.facebook_id = :self2) AND
+                  reserves.facebook_id = :friend
+            ', ["self1"=>$user, "self2"=>$user, "friend"=>$friend->id]);
+            array_push($match, $result);
+//            return $friend->{'id'};
+        }
+
+        return response()->json(["status"=>"ok", "data"=>$match]);
     }
 }
