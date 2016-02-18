218ac4c450c7715401967ba2c2d2ae316cf5387f
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-02-18 18:48:37 +0700

    add new migration user_lacation that collect the user latitude and longitude

---
+++ database/migrations/2016_02_18_113929_user_location.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserLocation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_location', function(Blueprint $table){
+            $table->increments("id");
+            $table->string("facebook_id");
+            $table->double("latitude");
+            $table->double("longitude");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_location');
+    }
+}
