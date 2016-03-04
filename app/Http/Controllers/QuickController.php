ceed5429da24181bf25e2f6fd94a330de0ae2257
Tossapon Nuanchuay <ntossapo@gmail.com>
2016-03-04 23:10:09 +0700

    fix bug QuickController get Quickmatch

--- app/Http/Controllers/QuickController.php
+++ app/Http/Controllers/QuickController.php
@@ -16,9 +16,8 @@ class QuickController extends Controller
         $user = Input::get("facebookId");
 
         $data = DB::select('
-            select reserves.*, stadiums.name as stadiumname, stadiums.latitude as latitude, stadiums.longitude as longitude, fields.name as fieldname, stadiums.image as image, users.name as username
-            , (select count(joins.facebook_id) from joins where joins.facebook_id = :facebook2) as isjoin
-            from reserves, stadiums, fields, users
+            select distinct reserves.*, stadiums.name as stadiumname, stadiums.latitude as latitude, stadiums.longitude as longitude, fields.name as fieldname, stadiums.image as image, users.name as username
+            from reserves, stadiums, fields, users, joins
             where
               users.facebook_id = reserves.facebook_id and
               reserves.facebook_id != :facebook and
@@ -26,8 +25,10 @@ class QuickController extends Controller
               fields.stadium_id = stadiums.id and
               fields.type = :type and
               reserves.isconfirm = 1 and
-              reserves.date >= curdate()
+              reserves.date >= curdate() and
+              (select count(*) from joins where joins.reserve_id = reserves.id and joins.facebook_id = :facebook2) != 1
               order by sqrt(pow(stadiums.latitude - :lat, 2) + pow(stadiums.longitude - :long, 2)) asc, reserves.date asc, reserves.time_from asc
+              limit 30
         ', ["lat"=>$lat, "long"=>$long, "type"=>$type, "facebook"=>$user, "facebook2"=>$user]);
 
         foreach($data as $key=>$val){
