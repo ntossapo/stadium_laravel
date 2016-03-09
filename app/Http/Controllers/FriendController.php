967732500e32b6a69bdc0109d96d7e5e60eb5091
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-03-09 16:41:12 +0700

    add import

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Input;
+use DB;
 class FriendController extends Controller
 {
     public function getFriendStat(){
